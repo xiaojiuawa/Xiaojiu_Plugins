8f51959603e0d8534361e6c86e705540b4373fa9
xiaojiuawa <xiaojiuawa123@qq.com>
2024-04-06 23:14:18 +0800

    更换下注册命令的部分

--- src/xiaojiu/commandExecutor/CommonExecutorLoader.java
+++ src/xiaojiu/commandExecutor/CommonExecutorLoader.java
@@ -3,12 +3,17 @@ package xiaojiu.commandExecutor;
 import xiaojiu.StartPlugins;
 
 public class CommonExecutorLoader {
+    public static PlayerCommand playerCommand = new PlayerCommand();
+    public static RestartServerCommand restartServerCommand = new RestartServerCommand();
+    public static SafeGuardCommand safeGuardCommand = new SafeGuardCommand();
+    public static SuggestCommand suggestCommand = new SuggestCommand();
+    public static PlayerTimeCommand playerTimeCommand = new PlayerTimeCommand();
     public static void Load(StartPlugins Instance) {
-        Instance.getCommand("restartserver").setExecutor(new RestartServerCommand());
-        Instance.getCommand("safeguard").setExecutor(new SafeGuardCommand());
-        Instance.getCommand("Limit").setExecutor(new PlayerCommand());
-        Instance.getCommand("Limit").setTabCompleter(new PlayerCommand());
-        Instance.getCommand("suggest").setExecutor(new SuggestCommand());
-        Instance.getCommand("playerTime").setExecutor(new PlayerTimeCommand());
+        Instance.getCommand("restartserver").setExecutor(restartServerCommand);
+        Instance.getCommand("safeguard").setExecutor(safeGuardCommand);
+        Instance.getCommand("Limit").setExecutor(playerCommand);
+        Instance.getCommand("Limit").setTabCompleter(playerCommand);
+        Instance.getCommand("suggest").setExecutor(suggestCommand);
+        Instance.getCommand("playerTime").setExecutor(playerTimeCommand);
     }
 }

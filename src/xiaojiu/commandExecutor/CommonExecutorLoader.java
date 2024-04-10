28fc2f8fc03f4279fc664216fbf576f1f432f023
xiaojiuawa <xiaojiuawa123@qq.com>
2024-04-10 10:35:44 +0800

    增加了TAB补全的注册部分

--- src/xiaojiu/commandExecutor/CommonExecutorLoader.java
+++ src/xiaojiu/commandExecutor/CommonExecutorLoader.java
@@ -11,10 +11,14 @@ public class CommonExecutorLoader {
 
     public static void Load(StartPlugins Instance) {
         Instance.getCommand("restartserver").setExecutor(restartServerCommand);
+        Instance.getCommand("restartserver").setTabCompleter(restartServerCommand);
         Instance.getCommand("safeguard").setExecutor(safeGuardCommand);
+        Instance.getCommand("safeguard").setTabCompleter(safeGuardCommand);
         Instance.getCommand("Limit").setExecutor(playerCommand);
         Instance.getCommand("Limit").setTabCompleter(playerCommand);
         Instance.getCommand("suggest").setExecutor(reloadTaskCommand);
+        Instance.getCommand("suggest").setTabCompleter(reloadTaskCommand);
         Instance.getCommand("playerTime").setExecutor(playerTimeCommand);
+        Instance.getCommand("playerTime").setTabCompleter(playerTimeCommand);
     }
 }

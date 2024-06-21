3da22262d663bf68633cef45a80767f7c2445db3
xiaojiuawa <xiaojiuawa123@qq.com>
2024-06-21 08:25:27 +0800

    把timer重启计时器取消内部类的设定

---
+++ src/main/java/com/github/xiaojiu/Handles/Restart/RestartServerUseTimer.java
@@ -0,0 +1,42 @@
+package com.github.xiaojiu.Handles.Restart;
+
+import com.github.xiaojiu.Xiaojiu;
+import com.github.xiaojiu.tools.MessageHelper;
+import com.github.xiaojiu.tools.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+import java.util.*;
+
+public class RestartServerUseTimer {
+    public static Timer timer;
+
+    public static void Restart(int time) {
+        timer = new Timer();
+        for (Map.Entry<Integer, String> entry : Utils.WaitMap.entrySet()) {
+            if (entry.getKey() > time) continue;
+            Date date = new Date();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            calendar.add(Calendar.SECOND, time - entry.getKey());
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (entry.getValue().equalsIgnoreCase("down")) {
+                        Bukkit.getScheduler().runTask(Xiaojiu.getInstance(), RestartTools::Done);
+                        //                        RestartHelper.Done();
+                    } else {
+                        MessageHelper.SendMessageAllPlayer(ChatColor.LIGHT_PURPLE + String.format(entry.getValue(), "重启"));
+                    }
+
+                }
+            }, calendar.getTime());
+        }
+    }
+
+    public static void cancel() {
+        if (timer == null) return;
+        timer.cancel();
+        timer = null;
+    }
+}
